0e81709164609122ed5193fce83603e690fcdba5
Sushant Sharma <sushant.wurk@gmail.com>
2024-10-05 02:37:58 +0530

    Ast 5.4 complete

--- crafting-interpreters/com.sushi.lox/src/main/java/com/sushi/lox/AstPrinter.java
+++ crafting-interpreters/com.sushi.lox/src/main/java/com/sushi/lox/AstPrinter.java
@@ -1,6 +1,18 @@
 package com.sushi.lox;
 
 class AstPrinter implements Expr.Visitor<String> {
+    public static void main(String[] args) {
+        Expr expr = new Expr.Binary(
+                new Expr.Unary(
+                        new Token(TokenType.MINUS, "-", null, 1),
+                        new Expr.Literal(123)),
+                new Token(TokenType.STAR, "*", null, 1),
+                new Expr.Grouping(new Expr.Literal(45.67))
+        );
+
+        System.out.println(new AstPrinter().print(expr));
+    }
+
     String print(Expr expr) {
         return expr.accept(this);
     }
